b929c5189b04bf1d0bab12135115d0ed0f7339f1
Pragya <pragyaprashar490@gmail.com>
2022-12-11 17:46:59 -0500

    installations

--- controllers/questions/questions-schema.js
+++ controllers/questions/questions-schema.js
@@ -4,6 +4,7 @@ const questionsSchema=mongoose.Schema({
     _id: Number,
     question: String,
     question_img: {data: Buffer, contentType: String},
+    time: String,
     user_id: String,
     genre:{type:Array, of: String},
     answers: {type: Array, of: Number}

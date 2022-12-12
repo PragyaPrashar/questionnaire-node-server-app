6c56d85033e45fc5ae69bc4a8361b4ea68e56a29
aishwaryakumara <aishwaryakumar.a4@gmail.com>
2022-12-11 23:42:15 -0500

    added mongo atlas connection link

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const MONGO_LOCAL='mongodb://localhost:27017/quans';
 const app=express()
 app.use(cors());
 app.use(express.json());
-mongoose.connect(MONGO_LOCAL);
+//mongoose.connect(MONGO_LOCAL);
+
+mongoose.connect('mongodb+srv://aishwaryakumara:Aishu1234@cluster0.len2zb0.mongodb.net/?retryWrites=true&w=majority');
+
 UserController(app);
 QuestionController(app);
 console.log("this is node")
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
